defddb24c0474d81749914aeed5f6229d0c967f3
Jeff Lupinski <lupinskij@gmail.com>
2015-09-23 17:19:03 -0400

    :rocket:

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,71 @@
 var gulp        = require('gulp'),
-    open        = require("gulp-open"),
     gutil       = require('gulp-util'),
     stylus      = require('gulp-stylus'),
-    nib         = require('nib'),
+    prefixer    = require('gulp-autoprefixer');
     minifyCSS   = require('gulp-minify-css'),
-    uglify      = require('gulp-uglify'),
     jade        = require('gulp-jade'),
+    changed     = require('gulp-changed'),
+    cached      = require('gulp-cached'),
+    filter      = require('gulp-filter'),
+    inherit     = require('gulp-jade-inheritance'),
+    coffee      = require('gulp-coffee'),
+    uglify      = require('gulp-uglify'),
     concat      = require('gulp-concat'),
-    rename      = require("gulp-rename"),
+    rename      = require('gulp-rename'),
     flatten     = require('gulp-flatten'),
-    marked      = require('marked'), // For :markdown filter in jade
+    marked      = require('marked'),
     path        = require('path'),
     plumber     = require('gulp-plumber'),
     notify      = require('gulp-notify'),
-    livereload  = require('gulp-livereload'),
-    tinylr      = require('tiny-lr'),
-    express     = require('express'),
-    app         = express(),
-    server      = tinylr(),
+    webserver   = require('gulp-webserver'),
     _if         = require('gulp-if'),
     isWindows   = /^win/.test(require('os').platform());
 
 // --- Stylus ---
 gulp.task('stylus', function () {
-    gulp.src('./dev/styles/*.styl')
-      .pipe(stylus({
-            use: ['nib'],
-            import: ['nib']
-            }))
-    .on('error', notify.onError({
-      title: 'Fail',
-      message: 'Stylus error'
-    }))
-    .on('error', function (err) {
-      return console.log(err);
-    })
-      .pipe(minifyCSS())
-      .pipe(concat("styles.css"))
-      .pipe(gulp.dest('./build/css'))
-        .pipe(livereload(server))
-        .pipe(_if(!isWindows, notify({
-          title: 'Sucess',
-          message: 'Stylus compiled'
-        })));
+  gulp.src('./dev/styles/**/*.styl')
+    .pipe(stylus())
+  .on('error', notify.onError({
+    title: 'Fail',
+    message: 'Stylus error'
+  }))
+  .on('error', function (err) {
+    return console.log(err);
+  })
+    .pipe(prefixer({browsers: ['last 2 versions']}))
+    .pipe(minifyCSS())
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('./build/css'))
+
+      .pipe(_if(!isWindows, notify({
+        title: 'Sucess',
+        message: 'Stylus compiled'
+      })));
 });
 
 // --- Normalize ---
 gulp.task('rename', function() {
-    gulp.src('bower_components/**/normalize.css')
-      .pipe(rename('normalize.styl'))
-      .pipe(gulp.dest('./dev/styles'));
+  gulp.src('bower_components/**/normalize.css')
+    .pipe(rename('normalize.styl'))
+    .pipe(gulp.dest('./dev/styles'));
+  gulp.src('bower_components/**/lt-ie-9.min.js')
+    .pipe(rename('lt-ie-9.js'))
+    .pipe(gulp.dest('./build/js/vendor'));
 });
 
 // --- Scripts ---
-gulp.task('js', function() {
-  return gulp.src('./dev/scripts/*.js')
+gulp.task('coffee', function() {
+  gulp.src('./dev/scripts/**/*.coffee')
+    .pipe(coffee({bare: true}))
+  .on('error', notify.onError({
+    title: 'Fail',
+    message: 'Coffee error'
+  }))
+  .on('error', function (err) {
+    return console.log(err);
+  })
     .pipe( uglify() )
     .pipe( gulp.dest('./build/js'))
-    .pipe(livereload(server))
     .pipe(_if(!isWindows, notify({
       title: 'Sucess',
       message: 'Javascript compiled'
@@ -67,20 +75,22 @@ gulp.task('js', function() {
 // --- Vendor ---
 gulp.task('vendor', function() {
   return gulp.src('bower_components/**/modernizr.js')
-      .pipe(flatten())
-      .pipe( uglify() )
-      // .pipe( concat('vendor.js'))
-      .pipe(gulp.dest('build/js'));
+    .pipe(flatten())
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js/vendor'));
 });
 
 // --- Jade ---
 gulp.task('templates', function() {
-  return gulp.src('./dev/*.jade')
-    .pipe(jade({
-      pretty: true
+  return gulp.src('./dev/**/*.jade')
+    .pipe(changed('dist', {extension: '.html'}))
+    .pipe(_if(global.isWatching, cached('jade')))
+    .pipe(inherit({basedir: './dev'}))
+    .pipe(filter(function(file) {
+      return !/\/_/.test(file.path) || !/^_/.test(file.relative);
     }))
+    .pipe(jade())
     .pipe(gulp.dest('./build'))
-    .pipe(livereload(server))
     .pipe(_if(!isWindows, notify({
       title: 'Sucess',
       message: 'Jade compiled'
@@ -89,29 +99,20 @@ gulp.task('templates', function() {
 
 // --- Server ---
 gulp.task('server', function() {
-  app.use(require('connect-livereload')());
-  app.use(express.static(path.resolve('./build')));
-  app.listen(4000);
-  gutil.log('Listening on localhost:4000');
-});
-
-// --- Open ---
-gulp.task('open', function(){
-  return gulp.src('./build/index.html')
-      .pipe(open('', {url:'http://localhost:4000'}));
+  gulp.src('./build')
+    .pipe(webserver({
+      livereload: true,
+      open: true,
+      port: 4000
+    }));
 });
 
 // --- Watch ---
-gulp.task('watch', function () {
-  server.listen(35729, function (err) {
-    if (err) {
-      return console.log(err);
-    }
-    gulp.watch('./dev/styles/*.styl',['stylus']);
-    gulp.watch('./dev/scripts/*.js',['js']);
-    gulp.watch('./dev/*.jade',['templates']);
-  });
+gulp.task('watch', function() {
+  gulp.watch(['./dev/styles/**/*.styl'], ['stylus']);
+  gulp.watch(['./dev/scripts/**/*.coffee'], ['coffee']);
+  gulp.watch(['./dev/**/*.jade'], ['templates']);
 });
 
 // --- Default task ---
-gulp.task('default', ['js','vendor','rename','stylus', 'templates','server','watch', 'open']);
+gulp.task('default', ['coffee','vendor','rename','stylus','templates','server','watch']);
